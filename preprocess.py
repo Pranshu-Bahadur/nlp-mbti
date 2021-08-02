7acf7b183ce351efbbfe0f0f7feecd98e4524694
Pranshu Bahadur <pranshu041@gmail.com>
2021-08-03 05:18:20 +0530

    Integration test: Generate tf2 Dataset

---
+++ preprocess.py
@@ -0,0 +1,125 @@
+from pandas import read_csv, DataFrame, concat, Series
+from functools import singledispatch, reduce
+from tqdm import tqdm
+from transformers import PreTrainedTokenizerBase
+import tensorflow.data.Dataset.from_tensor_slices as from_tensor_slices
+import numpy as np
+
+
+#======================================================================
+# 
+#======================================================================
+
+
+
+def generate_dataset(*args) -> Dataset:
+    """
+    Given a dictionary of instructions perform dynamic dispatch to pre-process
+    .csv as a dataframe based on given strategy args, encode DataFrame and 
+    return tf.data.Dataset object.
+    """
+    return reduce(lambda x: _generate_dataset_helper(**x), args)
+
+
+
+@singledispatch
+def _generate_dataset_helper(path : str, args : list) ->  DataFrame:
+    return nlp_tc_df_parser(path, *args)
+
+@_generate_dataset_helper.register
+def _tokenize(df : DataFrame, kwargs : dict) -> dict:
+    tokenizer = kwargs.pop('tokenizer')
+    encodings = tokenizer(list(df['posts']), **kwargs)
+    encodings['labels'] = df['labels']
+    return encodings
+
+@_generate_dataset_helper.register
+def _gen_tf_dataset(encodings : dict, kwargs=None) -> Dataset:
+    return from_tensor_slices(encodings)
+
+
+
+
+
+#======================================================================
+# Functions to preprocess a pandas.DataFrame from a csv
+#======================================================================
+
+
+def nlp_tc_df_parser(path : str, *args) -> DataFrame:
+    """
+    Given a path to an nlp text classification dataset (.csv), 
+    instantiate an instance of a DataFrame object and if needed perform
+    cleaning procedures on it according to given kwargs.
+
+    Parameters
+    ----------
+    path : str
+         path to dataset
+    *args : list
+         additional user args
+    Returns
+    -------
+    df : DataFrame
+        parsed DataFrame of in given path
+    """
+    data_frame = reduce(lambda x, y: _parser(y, x), args, read_csv(path)) if len(args) > 0 else read_csv(path)
+    return data_frame
+
+#======================================
+# Helper Functions for nlp_tc_df_parser:
+#======================================
+#TODO str labels to num
+# Remove hyperlinks that end with .com
+
+@singledispatch
+def _parser(strategy, df) -> DataFrame:
+    str_labels = pd.unique(df.labels.values.tolist())
+    labels_dict =  dict(zip(str_labels, list(range(len(str_labels)))))
+    df['labels'] = df['labels'].apply(lambda x: labels_dict[x])
+    return df
+
+
+@_parser.register
+def _hyper_link_cleaner(strategy : None, df)  -> DataFrame:
+    df.posts = df.posts.str.replace(r'(http|ftp|https)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?', " ")
+    return df
+
+@_parser.register
+def _remove_below_word_limit(strategy : int, df)  -> DataFrame:
+    df["total_words"] = df.posts.str.split(" ").map(len)
+    df = df[df["total_words"]>strategy]
+    df = df.drop(columns=["total_words"])
+    return df
+
+# Splits input rows based on a given delimiter
+@_parser.register
+def _explode(strategy : str, df)  -> DataFrame:
+    generic_col_names = ["labels", "x"]
+    df_col_names = df.columns.values.tolist()
+    df = df.rename(columns={df_col_names[i]: generic_col_names[i] for i in range(2)})
+    df = DataFrame(concat([Series(row['labels'], row['x'].split(strategy)) for _, row in tqdm(df.iterrows())])).reset_index()
+    generic_col_names.reverse()
+    df = df.rename(columns={k: generic_col_names[i] for i,k in enumerate(df.columns.values.tolist())})
+    return df
+
+  
+
+
+
+
+
+"""
+# Retaining domain name: doesn't transform multiple links in a single post
+@parser.register
+def domain_retain(strategy : list, df) -> DataFrame:
+    def transform_url(post):
+        url = re.search(r'\bhttp.*[a-zA-Z0-9]\s',post)
+        if url:
+            regex = re.findall(r'^.*\.(.*)\.', post)
+            post = post.replace(url.group(0),regex[0]+" ")
+        return post
+
+    df['posts'] = df['posts'].apply(lambda x: transform_url(x))
+    return df
+"""
--- test.py
+++ test.py
@@ -1,36 +1,36 @@
 import unittest
-from utils import parser, nlp_tc_df_parser
+from preprocess import _parser, nlp_tc_df_parser
 from pandas import read_csv
 
 PATH = 'mbti_1.csv'
 class TestDFParser(unittest.TestCase):
-    def test_parser_helpers(self):
+    def test__parser_helpers(self):
         path = PATH
         df = read_csv(path)
         
         #hyperlink remover check
-        no_links_df = parser(None, df)
-        self.assertFalse(no_links_df.posts.str.contains(r'\bhttp.*[a-zA-Z0-9]\b').any())
+        no_links_df = _parser(None, df)
+        self.assertFalse(no_links_df.posts.str.contains(r'(http|ftp|https)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?').any())
 
         #min word limit check
-        min_word_limit_df = parser(18, no_links_df)
+        min_word_limit_df = _parser(18, no_links_df)
         self.assertTrue(len(df)>len(min_word_limit_df))
 
-        #explosion check
-        e_df = parser("|||", df)
+        #.posts.losion check
+        e_df = _parser("|||", df)
         self.assertTrue(len(df)<len(e_df))
 
-        # Domain check
-        d_df = parser([], e_df)
-        self.assertTrue(len(d_df) == len(e_df))
+        # TODO Domain check
+        #d_df = _parser([], e_df)
+        #self.assertTrue(len(d_df) == len(e_df))
 
-    def test_nlp_tc_df_parser(self):
+    def test_nlp_tc_df__parser(self):
         path = PATH
         df = read_csv(path)
-        test_df = nlp_tc_df_parser(path, None, 2, "|||",[])
-        self.assertFalse(test_df.posts.str.contains(r'\bhttp*[a-zA-Z0-9]\b').any())
+        test_df = nlp_tc_df__parser(path, None, 2, "|||")
+        self.assertFalse(test_df.posts.str.contains(r'(http|ftp|https)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?').any())
         self.assertTrue(len(df)<len(test_df))
-        self.assertTrue(len(nlp_tc_df_parser(path))==len(df))
+        self.assertTrue(len(nlp_tc_df__parser(path))==len(df))
 
 
 #class Test
