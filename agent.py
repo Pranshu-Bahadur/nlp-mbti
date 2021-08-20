Accuracy calculation fixed

agent.py
@@ -1,18 +1,93 @@
-from proprocess import generate_dataset
-from transformers import TFAutoModelForSequenceClassification
+from preprocess import generate_dataset
+from transformers import AutoModelForSequenceClassification, Trainer, EvalPrediction
+import numpy as np
+from torch import nn
+import torch
+from torch.utils.data import WeightedRandomSampler
+from torch.utils.data import DataLoader as Loader
 
-def init_agent(name : str, path : str, num_labels : int, **kwargs) -> dict:
-    agent = {}
-    agent['model'] = TFAutoModelForSequenceClassification(name, num_labels=num_labels)
-    agent['model'].compile(**kwargs['agent_config'])
-    agent['dataset'] = generate_dataset(path, kwargs['dataset_config'])
-    agent['y'] = agent['dataset'].pop('labels')
-    agent['x'] = agent.pop('dataset')
+def init_agent(name : str, path : str, num_labels : int, train_split_factor : int, **kwargs) -> dict:
+    agent = {"model": AutoModelForSequenceClassification.from_pretrained(name, num_labels=num_labels).cuda()}
+    agent['train_dataset'],  agent['eval_dataset'], _ , _= _splitter(generate_dataset(path, kwargs['dataset_config']), train_split_factor)
     return agent
 
 def run(mode : str, agent : dict, **kwargs) -> dict:
-    model = agent.pop('model')
     kwargs = {**agent, **kwargs}
-    model.fit(**kwargs) if mode=="train" else model.evaluate(**kwargs)
-    agent['model'] = model
-    return agent
+    trainer = MultilabelTrainer(**kwargs, compute_metrics=_multilabel_accuracy) if kwargs.pop('multilabel') else SinglelabelTrainer(**kwargs, compute_metrics=_accuracy)
+    trainer.train()
+
+#TODO add the following methods to utils
+
+
+class SinglelabelTrainer(Trainer):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        loader = Loader(self.train_dataset, self.args.train_batch_size, shuffle=False, num_workers=4)
+        labels = torch.stack([x['labels'] for x in loader][:-1])
+        weights = torch.tensor([(labels==i).cpu().sum().item() for i in range(16)]).float()
+        weights /= weights.sum().item()
+        weights =  1 - weights
+        #weights[2:] = 0.5
+        #weights=torch.tensor([0.5, 0.5, 0.9, 0.9]).float()
+        #print(weights)
+        self.loss_fct = nn.CrossEntropyLoss(weight=weights).cuda()#pos_weight=weights
+    """
+    def compute_loss(self, model, inputs, return_outputs=False):
+        labels = inputs.pop("labels")
+        outputs = model(**inputs)
+        logits = outputs.logits
+        loss = self.loss_fct(logits.view(labels.size(0),-1), labels)
+        return (loss, outputs) if return_outputs else loss
+     """
+
+class MultilabelTrainer(Trainer):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        """
+        loader = Loader(self.train_dataset, self.args.train_batch_size, shuffle=False, num_workers=4)
+        labels = torch.stack([x['labels'] for x in loader][:-1])
+        weights = torch.tensor([labels[:,i].cpu().sum().item() for i in range(4)]).float()
+        weights /= weights.sum().item()
+        weights =  1 - weights
+        weights[2:] = 0.5
+        """
+        weights=torch.tensor([0.75, 0.75, 0.5, 0.5]).float()
+        #print(weights)
+        self.loss_fct = nn.BCEWithLogitsLoss(pos_weight=weights).cuda()#pos_weight=weights
+
+    def compute_loss(self, model, inputs, return_outputs=False):
+        labels = inputs.pop("labels")
+        outputs = model(**inputs)
+        logits = outputs.logits
+        loss = self.loss_fct(logits.view(-1, self.model.config.num_labels), labels.float())
+        return (loss, outputs) if return_outputs else loss
+
+    def create_optimizer(self):
+        self.optimizer = torch.optim.AdamW(self.model.parameters(), 1e-5, betas=(0.999, 0.9), weight_decay=1e5)
+        return self.optimizer
+
+
+def _splitter(dataset, *args):
+    train_split = int(args[0] * len(dataset))
+    eval_split = int(len(dataset) - train_split) // 2
+    splits = [train_split, *list(eval_split for _ in range(2))]
+    splits.append(len(dataset) - sum(splits))
+    return torch.utils.data.dataset.random_split(dataset, splits)
+
+def _multilabel_accuracy(outputs : EvalPrediction):
+    temp = 0
+    h, y_true = outputs.predictions, outputs.label_ids
+    h = torch.sigmoid(torch.from_numpy(h))
+    print(h[0])
+    #h[:,:-2] = torch.where(h[:,:-2] >= 0.6, 1, 0)
+    h = torch.where(h >= 0.5, 1, 0)
+    y_pred = h.long().cpu().detach().numpy()#np.vectorize(f)(h.unsqueeze(-1).cpu().detach().numpy())
+    for i in range(y_true.shape[0]):
+        temp += sum(np.logical_and(y_true[i], y_pred[i])) / sum(np.logical_or(y_true[i], y_pred[i]))
+    print(y_true.shape)
+    return {"accuracy" : temp/(y_true.shape[0])}
+
+def _accuracy(outputs : EvalPrediction):
+    y_pred, y_true = outputs.predictions, outputs.label_ids
+    accuracy = np.sum(np.argmax(y_pred, axis=-1) == y_true) / y_true.shape[0]
+    return {"accuracy" : accuracy}

preprocess.py
@@ -1,40 +1,53 @@
+import re
 from pandas import read_csv, DataFrame, concat, Series
 from functools import singledispatch, reduce
 from tqdm import tqdm
 from transformers import PreTrainedTokenizerBase
-from tensorflow import data
-from tensorflow.data import Dataset
 import numpy as np
 import pandas as pd
+from torch.utils.data import Dataset
+import torch
 
 #======================================================================
-# 
+#       Dataset preprocessing functions
 #======================================================================
 
 def generate_dataset(path : str, configs : list) -> Dataset:
     """
     Given a dictionary of instructions perform dynamic dispatch to pre-process
-    .csv as a dataframe based on given strategy args, encode DataFrame and 
+    .csv as a dataframe based on given strategy args, encode DataFrame and
     return tf.data.Dataset object.
     """
     return reduce(lambda x, y: _generate_dataset_helper(x, y), configs, path)
-
 
 @singledispatch
 def _generate_dataset_helper(path : str, args : list) ->  DataFrame:
     return nlp_tc_df_parser(path, *args)
 
+
 @_generate_dataset_helper.register
 def _tokenize(df : DataFrame, kwargs : dict) -> dict:
     tokenizer = kwargs.pop('tokenizer')
-    encodings = tokenizer(list(df['posts']), **kwargs)
-    encodings['labels'] = df['type'].tolist()
+    encodings = dict(tokenizer(list(df['posts'].values), **kwargs))
+    encodings['labels'] = list(df['type'].values)
     return encodings
 
-@_generate_dataset_helper.register
-def _gen_tf_dataset(encodings : dict, kwargs=None) -> Dataset:
-    return Dataset.from_tensor_slices(encodings)
+class EncodedDataset(Dataset):
+    def __init__(self, encodings):
+        self._labels = encodings.pop('labels')
+        self.encodings = encodings
 
+    def __getitem__(self, idx):
+        x = {k: torch.tensor(v[idx]) for k, v in self.encodings.items()}
+        x['labels'] = torch.tensor(self._labels[idx])
+        return x
+
+    def __len__(self):
+        return len(self._labels)
+
+@_generate_dataset_helper.register
+def _gen_tf_dataset(encodings : dict, kwargs : set) -> Dataset:
+    return EncodedDataset(encodings)
 
 
 #======================================================================
@@ -44,7 +57,7 @@ def _gen_tf_dataset(encodings : dict, kwargs=None) -> Dataset:
 
 def nlp_tc_df_parser(path : str, *args) -> DataFrame:
     """
-    Given a path to an nlp text classification dataset (.csv), 
+    Given a path to an nlp text classification dataset (.csv),
     instantiate an instance of a DataFrame object and if needed perform
     cleaning procedures on it according to given kwargs.
 
@@ -93,7 +106,7 @@ def _explode(strategy : str, df)  -> DataFrame:
     generic_col_names = ["labels", "x"]
     df_col_names = df.columns.values.tolist()
     df = df.rename(columns={df_col_names[i]: generic_col_names[i] for i in range(2)})
-    df = DataFrame(concat([Series(row['labels'], row['x'].split(strategy)) for _, row in tqdm(df.iterrows())])).reset_index()
+    df = DataFrame(concat([Series(row['labels'], row['x'].split(strategy)) for _, row in tqdm(df.iterrows())])).reset_index()#_splitter(row['x'], strategy, 128)
     df_col_names.reverse()
     df = df.rename(columns={k: df_col_names[i] for i,k in enumerate(df.columns.values.tolist())})
     df.to_csv("check.csv")
@@ -102,26 +115,14 @@ def _explode(strategy : str, df)  -> DataFrame:
 @_parser.register
 def _add_separate_cols(strategy: bool, df) -> DataFrame:
     df['type'] = df['type'].str.split('')
-    df['type'] = df['type'].apply(lambda x: list(map(lambda attr_type: 0 if attr_type in "INTJ" else 1, x[1:-1])))
+    df['type'] = df['type'].apply(lambda x: list(map(lambda attr_type: 0 if attr_type in "ESFP" else 1, x[1:-1])))
     return df
 
+def _splitter(string : str, delimiter : str, num_words : int) -> list:
+    string.replace(f"{delimiter}", ' ')
+    strings = string.split(' ')
+    return [' '.join(strings[j-num_words-1:j]) for j in range(num_words-1, len(strings), num_words)]
 
 
-
-
-"""
-Retaining domain name: doesn't transform multiple links in a single post
-@parser.register
-def domain_retain(strategy : list, df) -> DataFrame:
-    def transform_url(post):
-        url = re.search(r'\bhttp.*[a-zA-Z0-9]\s',post)
-        if url:
-            regex = re.findall(r'^.*\.(.*)\.', post)
-            post = post.replace(url.group(0),regex[0]+" ")
-        return post
-
-    df['posts'] = df['posts'].apply(lambda x: transform_url(x))
-    return df
-"""
 
 

test.py
@@ -1,19 +1,21 @@
 import unittest
 from preprocess import _parser, nlp_tc_df_parser, generate_dataset
 from pandas import read_csv
-from transformers import AutoTokenizer
+from transformers import AutoTokenizer, TrainingArguments
 from agent import init_agent, run
 import tensorflow as tf
 
 
 PATH = 'mbti_1.csv'
+
+"""
 class TestDFParser(unittest.TestCase):
     def test__parser_helpers(self):
         path = PATH
         df = read_csv(path)
-        
+
         #hyperlink remover check
-        no_links_df = _parser(None, df)
+        no_links_df = _parser(set(), df)
         self.assertFalse(no_links_df.posts.str.contains(r'(http|ftp|https)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?').any())
 
         #min word limit check
@@ -37,36 +39,44 @@ class TestDFParser(unittest.TestCase):
     def test_nlp_tc_df_parser(self):
         path = PATH
         df = read_csv(path)
-        test_df = nlp_tc_df_parser(path, ['bring me back pls.'], None, 2, "|||")
+        test_df = nlp_tc_df_parser(path, set(), "|||", 2, True)
         self.assertFalse(test_df.posts.str.contains(r'(http|ftp|https)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?').any())
         self.assertTrue(len(df)<len(test_df))
         self.assertTrue(len(nlp_tc_df_parser(path))==len(df))
 
-"""
 class TestGenDataset(unittest.TestCase):
     def test_gen_dataset(self):
         args = [[['', None, 2, "|||", True],
-                {"tokenizer": AutoTokenizer.from_pretrained("vinai/bertweet-base"), "max_length" : 40, "truncation" : True}]
+                {"tokenizer": AutoTokenizer.from_pretrained("distilbert-base-uncased"), "max_length" : 40, "truncation" : True}]
         generate_dataset(PATH, args)
 """
 
 class TestAgent(unittest.TestCase):
     def test_run_agent(self):
-        ds_config = [[set(), "|||", 2, True], 
-            {"tokenizer": AutoTokenizer.from_pretrained("vinai/bertweet-base"),
+        ds_config = [[set(), 2, "|||", True],
+            {"tokenizer": AutoTokenizer.from_pretrained("distilbert-base-uncased", use_fast=True),
             "max_length" : 40,
             "truncation" : True,
-            "padding" : "max_length"}]
-
-        agent_config = {"optimizer" : tf.keras.optimizers.Adam(learning_rate=176e-5),
-                        "loss" : tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-                        "metrics" : tf.metrics.SparseCategoricalAccuracy()}
-      
-        agent = init_agent("vinai/bertweet-base", PATH, 4, dataset_config=ds_config, agent_config=agent_config)
-        gpus = tf.config.list_logical_devices('GPU')
-        strategy = tf.distribute.MirroredStrategy(gpus)
-        with strategy.scope():
-            run("train", agent, batch_size=128, epochs=10, verbose='auto', validation_split=0.3, workers=4, use_multiprocessing=True)
+            "padding" : "max_length"}, set()]
+        train_args = {"do_train" : True,
+                      "per_device_train_batch_size" : 128,
+                      "per_device_eval_batch_size" : 128,
+                      "learning_rate" : 1e-03,
+                      "num_train_epochs" : 5,
+                      "logging_strategy" : "epoch",
+                      "seed" : 420,
+                      "output_dir" : "./ops",
+                      "do_eval" : True,
+                      "dataloader_num_workers" : 4,
+                      "evaluation_strategy" : "steps",
+                      "logging_dir" : "./logs",
+                      "logging_strategy" : "steps",
+                      "logging_steps" : 1000,
+                      }
+        train_args = TrainingArguments(**train_args)
+        #with train_args.strategy.scope():
+        agent = init_agent("distilbert-base-uncased", PATH, 4, dataset_config=ds_config, train_split_factor=0.6)
+        run("train", agent, args=train_args, multilabel=True)
 
 
 