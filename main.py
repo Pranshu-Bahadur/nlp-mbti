8031ce3f817d031fb162d22a07551bdf7e2267a2
mahidhar58 <tammineni.vardhan@learner.manipal.edu>
2021-08-20 15:59:00 +0530

    Updating the code

--- main.py
+++ main.py
@@ -130,7 +130,7 @@ if __name__ == "__main__":
 
         "logging_strategy": "steps",
 
-        "logging_steps": 100
+        "logging_steps": 1000
 
     }
     
@@ -142,4 +142,4 @@ if __name__ == "__main__":
     # Call the agent to initialize the model and run it
     _agent = agent.init_agent(agent_config['model'], agent_config['dataset_path'], agent_config['classes'], agent_config['train_split'], dataset_config= agent_config['dataset_config'])
 
-    agent.run("train", _agent, multilabel=model_config['multilabel'])
+    agent.run("train", _agent, args=train_args, multilabel=model_config['multilabel'])
