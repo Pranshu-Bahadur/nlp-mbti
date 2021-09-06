f7540c1892a088b7e8f84627dcf58b1d6b6575da
mahidhar58 <tammineni.vardhan@learner.manipal.edu>
2021-09-06 16:33:30 +0530

    f1 score

--- agent.py
+++ agent.py
@@ -14,9 +14,9 @@ def init_agent(name : str, path : str, num_labels : int, train_split_factor : in
 
 def run(mode : str, agent : dict, **kwargs) -> dict:
     kwargs = {**agent, **kwargs}
-    print(kwargs)
+    # print(kwargs)
     print(kwargs.pop("multilabel"))
-    trainer = MultilabelTrainer(**kwargs) # if kwargs.pop('multilabel') else SinglelabelTrainer(**kwargs, compute_metrics=_accuracy)
+    trainer = MultilabelTrainer(**kwargs, compute_metrics=_multilabel_accuracy) # if kwargs.pop('multilabel') else SinglelabelTrainer(**kwargs, compute_metrics=_accuracy)
     trainer.train()
 
 #TODO add the following methods to utils
